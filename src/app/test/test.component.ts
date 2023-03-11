dd0ac88f70b3dfe9ffff41652ebcf5d37adc7394
komalvhanmane <vhanmanekomal120603@gmail.com>
2023-03-11 23:48:31 +0530

    todo app complete

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -6,5 +6,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./test.component.css']
 })
 export class TestComponent {
+  tasks :any=[{
+    "todoName":"Task1",
+    "status":true
+  },
+  {
+    "todoName":"Task2",
+    "status":false
+  }
+  ]
 
+  ngOnInit(){
+
+  }
+
+  task :any={
+    "todoName":'',
+    "status":false
+  }
+
+  public addTask(){
+    console.log("Hello")
+    this.tasks.push(this.task);
+    this.task={
+    }
+  }
+
+  public deleteTask(taskName:any)
+  {
+    const index = this.tasks.indexOf(taskName);
+    const x = this.tasks.splice(index, 1);
+    // this.tasks.slice(taskName)
+    console.log(index)
+    console.log(x)
+  }
+
+  public finish(item:any){
+    const index = this.tasks.indexOf(item);
+    this.tasks[index].status=true;
+  }
 }
